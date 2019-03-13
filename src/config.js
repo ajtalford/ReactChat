15c27e98cc787f936a1876fb49a7b52641d857e7
ajtalford <ajtalford@gmail.com>
2019-03-12 21:32:37 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/5909b13f-cb14-44b3-9e4e-82ef547fbe97/token";
+const instanceLocator = "v1:us1:5909b13f-cb14-44b3-9e4e-82ef547fbe97";
+
+export { tokenUrl, instanceLocator }
